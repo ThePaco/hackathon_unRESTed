3e1b39a7f5f8ea2478f9d2ffb87ceef4ac3f7d91
ThePaco <pv_hr@hotmail.com>
2022-03-19 13:29:05 +0100

    fixed person in getUsersByID

--- backend/model/Crud.py
+++ backend/model/Crud.py
@@ -7,9 +7,6 @@ from backend.model.Models import Reservation, Team
 from model import Models
 from model.Schemas import CreateUser, PatchUserForm
 
-def getUserByID(db: Session, publicId: int):
-    return db.query(Models.User).filter(Models.User.publicId == publicId).first()
-
 def createUser(user: CreateUser, db: Session):
     passw_encoded = user.password.encode()
     password_sha256 = hashlib.sha256(passw_encoded).hexdigest()
@@ -19,6 +16,15 @@ def createUser(user: CreateUser, db: Session):
     db.commit()
     return dbUser
 
+def updateUser():
+    pass
+
+def getUserByID(db: Session, publicId: int):
+    return db.query(Models.Person).filter(Models.Person.publicId == publicId).first()
+
+def getAllUsers():
+    pass
+
 def createTeam(team: Team, db:Session):
     public_id = str(uuid.uuid4())
     dbTeam = Models.Team(publicId = public_id, teamName = team.teamName)
@@ -26,11 +32,15 @@ def createTeam(team: Team, db:Session):
     db.commit()
     return dbTeam
 
+def updateTeam():
+    pass
+
+def getAllTeams():
+    pass
+
 def createReservation(reservation: Reservation, db: Session):
     public_id = str(uuid.uuid4())
     dbReservation = Models.Reservation(publicId = public_id, roomId = reservation.roomId, reservationStart = reservation.reservationStart, reservationEnd = reservation.reservationEnd)
     db.add(dbReservation)
     db.commit()
     return dbReservation
-
-#promjena3
